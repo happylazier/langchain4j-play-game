1fc16d5ce3855a430f165f5b6627924ee2ec340d
happylazier <399790262@qq.com>
2026-04-06 21:30:25 +0800

    无事发生

--- src/main/java/org/example/aicode/aiservice/Aiservice.java
+++ src/main/java/org/example/aicode/aiservice/Aiservice.java
@@ -2,6 +2,7 @@ package org.example.aicode.aiservice;
 
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
@@ -17,5 +18,6 @@ import reactor.core.publisher.Flux;
 )
 public interface Aiservice {
 
+    @SystemMessage("你是一个聊天机器人，协助用户解答问题，提供信息，并让你的语言尽量有趣")
     Flux<String> chat(@UserMessage String messages, @MemoryId String memoryId);
 }
